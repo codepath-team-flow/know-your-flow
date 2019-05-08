19f3d407676ea28938d1c1710995e1130c47ba65
Sophia <qyoung@ucsd.edu>
2019-05-07 19:52:15 -0700

    FSCalendar init

---
+++ know-your-flow/know-your-flow/CalendarViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CalendarViewController.swift
+//  know-your-flow
+//
+//  Created by Sophia Sun on 5/7/19.
+//  Copyright © 2019 debbienvo. All rights reserved.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController {
+    
+//    fileprivate weak var calendar: FSCalendar!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // In loadView or viewDidLoad
+//        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
+//        calendar.dataSource = self
+//       calendar.delegate = self
+//        view.addSubview(calendar)
+//        self.calendar = calendar
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+//
+//extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegate {
+//
+//}
