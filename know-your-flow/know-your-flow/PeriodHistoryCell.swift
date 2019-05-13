eb10663e2728ff99a327b68615826455bb185f41
Olivia Thai <olivia.d.thai@gmail.com>
2019-05-13 02:32:14 -0700

    history: can delete dates

---
+++ know-your-flow/know-your-flow/PeriodHistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeriodHistoryCell.swift
+//  know-your-flow
+//
+//  Created by Olivia Thai on 5/12/19.
+//  Copyright © 2019 debbienvo. All rights reserved.
+//
+
+import UIKit
+
+class PeriodHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
