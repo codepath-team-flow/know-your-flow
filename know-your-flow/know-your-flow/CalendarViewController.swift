517f03916912592f504645c140ea9aaddcfdb871
Sophia <qyoung@ucsd.edu>
2019-05-22 18:35:50 -0700

    AutoLayout: removed all Warnings

--- know-your-flow/know-your-flow/CalendarViewController.swift
+++ know-your-flow/know-your-flow/CalendarViewController.swift
@@ -12,11 +12,12 @@ import Parse
 
 class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource,FSCalendarDelegateAppearance {
     
-//    fileprivate weak var calendar: FSCalendar!
+    //    fileprivate weak var calendar: FSCalendar!
     
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var selectedDateLabel: UILabel!
-
+    @IBOutlet weak var eventDiscriptionLabel: UILabel!
+    
     let dateFormatter = DateFormatter()
     var periodDays = [Date]()
     var fertileDays = [Date]()
@@ -33,7 +34,7 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
         calendar.appearance.borderRadius = 0
         calendar.appearance.headerMinimumDissolvedAlpha = 0
         
-//        calendar.register(FSCalendarCell.self, forCellReuseIdentifier: "CELL")
+        //        calendar.register(FSCalendarCell.self, forCellReuseIdentifier: "CELL")
         
         // append event date to array
         let query = PFQuery(className: "PeriodHistory")
@@ -61,7 +62,7 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
                     //self.eventDays.append(record["startDate"] as! Date)
                     //self.eventDays.append(record["endDate"] as! Date)
                 }
-              
+                
                 self.calendar.reloadData()
             }
             
@@ -69,32 +70,32 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
     }
     
     
-// Change subtitle of cell
-//    func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
-//        if (calendar.today==date) {
-//            return "today";
-//        }
-//        return nil
-//    }
-
-
-//    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-//        if periodDays.contains(date){
-//            return 1
-//        }
-//        return 0
-//    }
+    // Change subtitle of cell
+    //    func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
+    //        if (calendar.today==date) {
+    //            return "today";
+    //        }
+    //        return nil
+    //    }
     
     
-//    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
-//
-//        if periodDays.contains(date) {
-//            return UIColor(displayP3Red: 1.0, green: 0.5864, blue: 0.54129, alpha: 0.6)
-//            // red = "1.0" green="0.5864" blue="0.54129"
-//
-//        }
-//        return nil
-//    }
+    //    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+    //        if periodDays.contains(date){
+    //            return 1
+    //        }
+    //        return 0
+    //    }
+    
+    
+    //    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
+    //
+    //        if periodDays.contains(date) {
+    //            return UIColor(displayP3Red: 1.0, green: 0.5864, blue: 0.54129, alpha: 0.6)
+    //            // red = "1.0" green="0.5864" blue="0.54129"
+    //
+    //        }
+    //        return nil
+    //    }
     
     
     
@@ -102,14 +103,23 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         self.dateFormatter.dateFormat = "MMM d, yyyy"
         self.selectedDateLabel.text = self.dateFormatter.string(for: date)
+        
+        if periodDays.contains(date){
+            self.eventDiscriptionLabel.text = "Period Day"
+            
+        }else if fertileDays.contains(date){
+            self.eventDiscriptionLabel.text = "Fertility Day"
+        }else{
+            self.eventDiscriptionLabel.text = ""
+        }
     }
     
     
     
- //Change image of cell
+    //Change image of cell
     func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
         if periodDays.contains(date){
-
+            
             return UIImage(named: "pink-line-32")
         }else if (fertileDays.contains(date)){
             return UIImage(named: "blue-line-32")
@@ -117,20 +127,20 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
         return nil
     }
     
-// Register Cell
-//    func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
-//
-//        let cell = calendar.dequeueReusableCell(withIdentifier: "CELL", for: date, at: position)
-//        cell.imageView.contentMode = .scaleAspectFit
-//        return cell
-//    }
+    // Register Cell
+    //    func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
+    //
+    //        let cell = calendar.dequeueReusableCell(withIdentifier: "CELL", for: date, at: position)
+    //        cell.imageView.contentMode = .scaleAspectFit
+    //        return cell
+    //    }
     
     // FSCalendarDelegate
-//    func calendar(calendar: FSCalendar!, didSelectDate date: NSDate!) {
-//        dateFormatter.dateFormat = "MMM d, yyyy"
-//        dateLabel.text = dateFormatter.string(from: date as Date)
-//    }
-//
+    //    func calendar(calendar: FSCalendar!, didSelectDate date: NSDate!) {
+    //        dateFormatter.dateFormat = "MMM d, yyyy"
+    //        dateLabel.text = dateFormatter.string(from: date as Date)
+    //    }
+    //
 }
 
 
